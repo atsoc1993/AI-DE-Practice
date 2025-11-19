f97269c95918a9eb3cb18cfed1bd1b242132a14e
atsoc1993 <atsoc1993@gmail.com>
2025-11-19 02:15:12 -0500

    Added arbitrary globals and test functions

--- avm_bytecode_to_breakdown/contract.py
+++ avm_bytecode_to_breakdown/contract.py
@@ -1,10 +1,28 @@
-from algopy import ARC4Contract, log
+from algopy import ARC4Contract, log, Bytes
 from algopy.arc4 import abimethod
 
 class TestContract(ARC4Contract):
     def __init__(self) -> None:
-        pass
+        self.zxzxzxzxzxzx= Bytes(b'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC')
+        self.zxzxzxzxzxzxzxzxzx = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
+        self.zxzxzxzxzxzxzxzxzxzxzxzx = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
+        self.zxzxzxzxzxzxzxzxzxzxzxxzzxxzzx = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
+        self.zxzxzxzxzxzxzxzxzxzxzxxzxzxzzzxz = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
+        self.zxzxzxzxzxzxzxzxzxxzxxzxzxzzzxzxzxxzxzxzzzxz = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
+        self.zxzxzxzxzxzxzxzxzxxzxxzxzxzzzxzxzxxzxzxzzzxzxzxxzxzxzzzxz = Bytes(b'AAAAAAAAAAAAAAAAAAAA')
     
     @abimethod
-    def test(self) -> None:
+    def test1(self) -> None:
+        log("Test")
+        
+    @abimethod
+    def test2(self) -> None:
+        log("Test")
+
+    @abimethod
+    def test3(self) -> None:
+        log("Test")
+
+    @abimethod
+    def test4(self) -> None:
         log("Test")
